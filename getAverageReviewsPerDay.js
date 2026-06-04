728b40c2da78b0ded987ffa90a42f4af029f26c2
Ian Goodacre <IanGoodacre3@gmail.com>
2026-06-05 05:38:25 +1200

    Fix getAverageReviewsPerDay

--- getAverageReviewsPerDay.js
+++ getAverageReviewsPerDay.js
@@ -1,10 +1,11 @@
 'use strict';
+const formatLocalDate = require('./formatLocalDate');
 
 module.exports = function getAverageReviewsPerDay (days = 14) {
   const self = this;
 
   const hist = self.db.prepare(`
-    select count() as n
+    select revdate, count() as n
     from revlog
     group by revdate
     order by revdate desc
@@ -13,7 +14,12 @@ module.exports = function getAverageReviewsPerDay (days = 14) {
   .all(days + 1);
 
   if (hist.length === 0) return 0;
-  if (hist.length > 1) hist.shift();
+  if (
+    hist.length > 1 &&
+    hist[0].revdate === formatLocalDate(new Date())
+  ) {
+    hist.shift();
+  }
   let sum = 0;
   hist.forEach(record => {
     sum += record.n;
