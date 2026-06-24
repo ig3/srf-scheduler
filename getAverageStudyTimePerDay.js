ac94919a6c9aa8059e77d389d72c0b733648fb47
Ian Goodacre <IanGoodacre3@gmail.com>
2026-06-25 11:54:29 +1200

    Change getAverageStudyTimePerDay back to linear average.
    Linear average is much simpler to calculate and most of the time
    provides nearly the same result. In a steady state, very little
    difference. A little slower to change in response to changes in time per
    day, but I also changed the default window back to 7 days from 14 days,
    making it more responsive to recent data. In balance, it should be good
    enough and less work/complexity.

--- getAverageStudyTimePerDay.js
+++ getAverageStudyTimePerDay.js
@@ -1,7 +1,7 @@
 'use strict';
-// Returns the exponentially weighted moving average of study time
+// Returns the average study time per day over the given number of days,
 // as seconds per day.
-module.exports = function getAverageStudyTimePerDay (days = 14) {
+module.exports = function getAverageStudyTimePerDay (days = 7) {
   if (!this.days || this.days < days) {
     const firstID = this.db.prepare(`
       select min(id) as id from revlog
@@ -11,27 +11,13 @@ module.exports = function getAverageStudyTimePerDay (days = 14) {
     days = Math.min(days, this.days);
   }
 
-  const series = this.db.prepare(`
-    select
-      sum(studytime) as time,
-      cast((unixepoch() - id/1000)/86400 as integer) as day
-    from revlog
-    where day < ?
-    group by day
-    order by day
-  `)
-  .all(days);
-
-  // There may be days without study: build an array indexed by days ago
-  const times = [];
-  series.forEach(record => {
-    times[record.day] = record.time;
-  });
-
-  let avg = times[days - 1] || 0;
-  const alpha = 2 / (days + 1);
-  for (let n = days - 2; n >= 0; n--) {
-    avg = alpha * (times[n] || 0) + (1 - alpha) * avg;
-  }
-  return Math.floor(avg);
+  return Math.floor(
+    this.db.prepare(`
+      select
+        sum(studytime) as time
+      from revlog
+      where id > ?
+    `)
+    .get((new Date()) - 86400000 * days).time / days
+  );
 };
