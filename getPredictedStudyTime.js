04edf7b47a9b65172063a135516a589d3370ae96
Ian Goodacre <IanGoodacre3@gmail.com>
2026-06-29 08:50:56 +1200

    Add caching to getPredictedStudyTime

--- getPredictedStudyTime.js
+++ getPredictedStudyTime.js
@@ -3,11 +3,21 @@
 // Based on cards due, average new cards per day and average study time per
 // card.
 module.exports = function getPredictedStudyTime () {
-  return Math.min(
+  if (this.getPredictedStudyTimeCache !== undefined) {
+    return this.getPredictedStudyTimeCache;
+  }
+  this.getPredictedStudyTimeCache = Math.min(
     86400,
     Math.round(
       (this.getCardsDue(86400) + this.getAverageNewCardsPerDay()) *
         this.getAverageStudyTimePerCard()
     )
   );
+  setTimeout(
+    () => {
+      delete this.getPredictedStudyTimeCache;
+    },
+    50
+  );
+  return this.getPredictedStudyTimeCache;
 };
