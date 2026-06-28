d4f882ecf8363eaccf7fc4f4c1ee098119b6515a
Ian Goodacre <IanGoodacre3@gmail.com>
2026-06-29 08:56:11 +1200

    Add caching to getAverageReviewsPerCard

--- getAverageReviewsPerCard.js
+++ getAverageReviewsPerCard.js
@@ -4,6 +4,9 @@
 // studied every day in the window. Therefore, the average must first be
 // determined on a daily basis.
 module.exports = function getAverageReviewsPerCard (days = 14) {
+  if (this.getAverageReviewsPerCardCache !== undefined) {
+    return this.getAverageReviewsPerCardCache;
+  }
   const hist = this.db.prepare(`
     select
       revdate,
@@ -14,11 +17,21 @@ module.exports = function getAverageReviewsPerCard (days = 14) {
     order by revdate desc
     limit ?
   `).all(days + 1);
-  if (hist.length === 0) return (10);
-  if (hist.length > 1) hist.shift();
-  let sum = 0;
-  hist.forEach(record => {
-    sum += record.reviews / record.cards;
-  });
-  return (sum / hist.length);
+  if (hist.length === 0) {
+    this.getAverageReviewsPerCardCache = 10;
+  } else {
+    if (hist.length > 1) hist.shift();
+    let sum = 0;
+    hist.forEach(record => {
+      sum += record.reviews / record.cards;
+    });
+    this.getAverageReviewsPerCardCache = (sum / hist.length);
+  }
+  setTimeout(
+    () => {
+      delete this.getAverageReviewsPerCardCache;
+    },
+    50
+  );
+  return this.getAverageReviewsPerCardCache;
 };
